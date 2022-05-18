9f7134f2a48c08df1bea3cf504347487c0f2e96f
evilbeast <houshao55@gmail.com>
2022-05-19 02:54:13 +0800

    初始化环境

---
+++ pyxcgui/main.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "main.h"
+
+
+#include <pybind11/pybind11.h>
+
+
+namespace py = pybind11;
+
+
+int add(int i, int j) {
+	return i + j;
+}
+
+PYBIND11_MODULE(xcgui, m) {
+	m.doc() = "pybind11 example plugin"; // optional module docstring
+
+	m.def("add", &add, "A function that adds two numbers");
+}
---
+++ pyxcgui/pch.h
@@ -0,0 +1,12 @@
+﻿#ifndef PCH_H
+#define PCH_H
+
+// 添加要在此处预编译的标头
+#include "framework.h"
+
+#pragma comment(lib, "libminizip.lib")
+#pragma comment(lib, "zlib.lib")
+#pragma comment(lib, "XCGUI-v3.3.4.lib")
+
+#endif 
+
